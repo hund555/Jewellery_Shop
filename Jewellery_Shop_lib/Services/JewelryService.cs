39d00a88d0df00a5c608260446396308946496dd
hund555 <allanholmiversen@gmail.com>
2025-10-08 08:38:37 +0200

    Added JewelryService

---
+++ Jewellery_Shop_lib/Services/JewelryService.cs
@@ -0,0 +1,20 @@
+﻿using Jewellery_Shop_lib.Domain;
+using Jewellery_Shop_lib.File_service;
+using Jewellery_Shop_lib.Services.Service_Interfaces;
+
+namespace Jewellery_Shop_lib.Services
+{
+    internal class JewelryService : IJewelryService
+    {
+        private IFileRepository _fileRepository;
+        public JewelryService()
+        {
+            _fileRepository = FileRepository.GetInstance();
+        }
+
+        public List<Jewelry> GetAllJewelryItems()
+        {
+            return _fileRepository.GetAllJewelry();
+        }
+    }
+}
